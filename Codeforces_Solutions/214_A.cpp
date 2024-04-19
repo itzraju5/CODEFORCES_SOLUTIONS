9e3433ee2f3d1239d03624eeac64b33b81c721e0
Raju Kumar Thakur <rajukumarthakur2001@gmail.com>
2024-04-19 22:09:44 +0530

    214A - System of Equations

---
+++ Codeforces_Solutions/214_A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n, m;
+   cin >> n >> m;
+   int cnt(0);
+   for(int a=0; a*a<=n && a<=m; a++)
+   {
+       int b = n- a*a;
+       if(a+b*b == m) cnt++;
+   }
+   cout << cnt;
+   
+   return 0;
+}
