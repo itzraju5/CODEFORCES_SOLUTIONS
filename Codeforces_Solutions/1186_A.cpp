87eb2fab06bb84631853817955830a6b2d6dd578
Raju Kumar Thakur <rajukumarthakur2001@gmail.com>
2024-04-24 23:13:27 +0530

    1186A - Vus the Cossack and a Contest

---
+++ Codeforces_Solutions/1186_A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m,k;
+    cin >> n >> m >> k;
+    
+    if(min(m,k) >= n)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
