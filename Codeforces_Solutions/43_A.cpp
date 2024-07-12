937af95bce525cb6f73265f17e1bca0d60975d75
Raju Kumar Thakur <rajukumarthakur2001@gmail.com>
2024-07-12 20:57:53 +0530

    43A - Football

---
+++ Codeforces_Solutions/43_A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    map<string, int> mp;
+    while(n--)
+    {
+        string s;
+        cin >> s;
+        mp[s]++;
+    }
+    int res(0);
+    string ans;
+    for(auto i:mp)
+    {
+        if(i.second > res)
+        {
+            res = i.second;
+            ans = i.first;
+        }
+    }
+    cout << ans;
+    
+    return 0;
+}
+
