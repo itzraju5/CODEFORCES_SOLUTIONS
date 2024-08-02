5d7605ef02d7ca3c11f8733396fa1902b62ea032
Raju Kumar Thakur <rajukumarthakur2001@gmail.com>
2024-08-02 22:25:00 +0530

    .\.git\

---
+++ Codeforces_Solutions/1144_A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool solve(string s)
+{
+    set<char>st;
+    for(int i=0; i<s.length(); i++) st.insert(s[i]);
+        
+    if(st.size() != s.length()){
+        return false;
+    }
+    sort(s.begin(), s.end());
+    for(int i=1; i<s.length(); i++)
+    {
+        if(abs(s[i]-s[i-1]) != 1){
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        
+        if(solve(s)){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
