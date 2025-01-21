8fcc3d5d312fb0d4826878561d44e2f37f109331
Raju Kumar Thakur <rajukumarthakur2001@gmail.com>
2025-01-21 23:04:41 +0530

    1450A - Avoid Trygub

---
+++ Codeforces_Solutions/1450_A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+        
+        string s;
+        cin >> s;
+        
+        sort(s.begin(), s.end());
+        cout << s << endl;
+    }
+    
+    return 0;
+}
+
+
