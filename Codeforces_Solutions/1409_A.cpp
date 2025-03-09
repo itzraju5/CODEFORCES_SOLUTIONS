71c307e1b0f5f6edd36772a5fdfa9fc124b395ee
Raju Kumar Thakur <rajukumarthakur2001@gmail.com>
2025-03-09 23:20:12 +0530

    1409A - Yet Another Two Integers Problem

---
+++ Codeforces_Solutions/1409_A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        
+        int diff = abs(a - b);
+        int moves = ceil(diff / 10.0); 
+        cout << moves << endl;
+    }
+    
+    return 0;
+}
+
